b3e3ddd6fe48a039f19f81829be3b4caa620d204
benwelner <benjaminrosswelner@gmail.com>
2026-05-10 22:38:02 +0000

    scraper fix

--- src/scraper.mjs
+++ src/scraper.mjs
@@ -144,7 +144,7 @@ async function syncLeagues() {
     new UniversalStaticAdapter('WEC', '🏎️', 'wec'),
     new UniversalStaticAdapter('IMSA', '🏎️', 'imsa'),
     new UniversalStaticAdapter('SUPERCARS', '🏎️', 'supercars'),
-    new UniversalStaticAdapter('NÜRBURGRING 24H', '🏁', 'n24')
+    new UniversalStaticAdapter('NÜRBURGRING 24H', '🏁', 'nurburgring') // Fixed folder path
   ];
   
   const uniqueEvents = new Map();
