f91fcb2e0504ddd369daeb3bead5287a2869f988
benwelner <benjaminrosswelner@gmail.com>
2026-06-12 03:38:34 +0000

    ARCA Update

--- src/app/page.js
+++ src/app/page.js
@@ -36,7 +36,7 @@ const FAVORITE_LOGOS = {
   "FORMULA 2": "https://raw.githubusercontent.com/benwelner/bw_sports/main/src/app/_images/logos/f2.png",
   "FORMULA 3": "https://raw.githubusercontent.com/benwelner/bw_sports/main/src/app/_images/logos/f3.png",
   "F1 ACADEMY": "https://raw.githubusercontent.com/benwelner/bw_sports/main/src/app/_images/logos/f1-academy.png",
-  "CARVANA PPA TOUR": "https://raw.githubusercontent.com/benwelner/bw_sports/main/src/app/_images/logos/ppa-tour.png"
+  "CARVANA PPA TOUR": "https://raw.githubusercontent.com/benwelner/bw_sports/main/src/app/_images/logos/ppa-tour.png",
   "ARCA MENARDS": "https://raw.githubusercontent.com/benwelner/bw_sports/main/src/app/_images/logos/arca_menards.png"
 };
 
